bec53b0a62ed5067f5bf507c5149a0f767a641f5
Marc Thalen <marcthalen@gmail.com>
2023-09-19 17:31:32 -0600

    wallet connect with web3modal

--- webapp/src/components/NavBar.vue
+++ webapp/src/components/NavBar.vue
@@ -1,9 +1,126 @@
-<script></script>
+<script>
+import { getAccount, waitForTransaction, readContract, writeContract, watchAccount, watchNetwork } from '@wagmi/core'
+import { useWeb3Modal, createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/vue'
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        let account = getAccount()
+        let modal = useWeb3Modal()
+        let accountActive = ref(false)
+
+        function openWalletModal() {
+            modal.open()
+        }
+
+        watchAccount(async (account) => {
+        if(account.isConnected == true) {
+            accountActive.value = true;
+        } else {
+            accountActive.value = false
+        }
+    })
+
+        return { account, openWalletModal, accountActive} 
+    }
+    
+}
+</script>
 
 <template>
-    <div>
-        <h2>Navbar</h2>
+    <div class="navbar">
+        <a style="cursor: pointer;" href="/"><div class="logo">
+            <h2>Verse Labs</h2>
+        </div></a>
+        <div class="links">
+            <ul>
+                <li><a href="/" >Get Ticket</a></li>
+                <li><a href="/tickets">View Tickets</a></li>
+            </ul>
+        </div>
+        <div class="wallet">
+            <button class="btn btn-connect" v-if="!accountActive" @click="openWalletModal">Connect Wallet</button>
+            <w3m-button v-if="accountActive"  />
+        </div>
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+.btn-connect {
+    margin-top: 2px;
+    border: none;
+    width: 140px;
+    border-radius: 10px;
+    font-weight: 600;
+    padding: 5px 20px;
+    height: 40px;
+    background-color: transparent;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+}
+
+    .navbar {
+        background-color: rgba(22, 22, 34, 0.8117647059);
+        width: 100%;
+        height: 70px;
+        div.logo {
+            color: white;
+            padding-left: 30px;
+            width: 32%;
+            margin: 0;
+            float: left;
+            @media(max-width: 880px) {
+                width: 100%;
+            }
+ 
+        }
+        div.links { 
+            @media(max-width: 880px) {
+            width: 100%!important;
+            }
+            margin: 0;
+            padding-top: 10px;
+            float: left;
+            width: 32%;
+            text-align: center;
+            ul {
+            display: inline-block;
+            margin-left: 0 auto;
+            list-style-type: none;
+                li {
+                    
+                    float: left;
+                    margin-right: 20px;
+
+                    a{
+                        text-decoration: none;
+                        color: #c6bfff;
+                        font-weight: 500;
+                    }
+                }
+            }
+        }
+        div.wallet {
+            @media(max-width: 930px) {
+                position: absolute;
+                bottom: 16px;
+                width: 400px;
+                left: 10px;
+            }
+            margin: 0;
+            margin-top: 10px;
+            margin-right: 10px;
+            float: right;
+            padding-top: 5px;
+            text-align: right;
+
+            h5 {
+                font-weight: 400;
+                color: #c6bfff;
+                margin-right: 20px;
+            }
+        }
+    }
+</style>
--- webapp/src/pages/HomePage.vue
+++ webapp/src/pages/HomePage.vue
@@ -1,6 +1,23 @@
 <script>
+    import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/vue'
+    import { polygon } from '@wagmi/core/chains'
     import { ref } from 'vue';
-</script>
+
+    const projectId = 'b30bc40c0cdef6000cd5066be1febf74'
+    const chains = [polygon]
+    const wagmiConfig = defaultWagmiConfig({ chains, projectId, appName: 'Verse Labs',  })
+
+    createWeb3Modal({ 
+        tokens: {
+            137:{
+                address:"0xc708d6f2153933daa50b2d0758955be0a93a8fec",
+                image:"https://assets.coingecko.com/coins/images/28424/small/verselogo.png?1670461811" 
+            },
+        
+        },
+        includeWalletIds: ['107bb20463699c4e614d3a2fb7b961e66f48774cb8f6d6c1aee789853280972c','c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', '19177a98252e07ddfc9af2083ba8e07ef627cb6103467ffebb3f8f4205fd7927'], wagmiConfig, projectId, chains})
+    
+    </script>
 
 
 <template>
