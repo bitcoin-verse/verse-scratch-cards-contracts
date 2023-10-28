effe2088db5d0dffd84e27573d83cd555e764d44
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-10-28 19:35:09 +0700

    lock version to =0.8.21

--- contracts/ScratchVRF.sol
+++ contracts/ScratchVRF.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+
+pragma solidity =0.8.21;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
