9d9941add456fba51ffb08e9834994b782720ec2
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-10-28 19:38:59 +0700

    cleanup

--- contracts/ScratchVRF.sol
+++ contracts/ScratchVRF.sol
@@ -25,7 +25,6 @@ contract ScratchVRF is Ownable, VRFConsumerBaseV2 {
     bytes32 constant public GAS_KEYHASH = 0xcc294a196eeeb44da2888d17c0625cc88d70d9760a69d58d853ba6581a9ab0cd;
     address constant public TOKEN_ADDRESS = 0xc708D6F2153933DAA50B2D0758955Be0A93A8FEc;
 
-    uint256 currentTokenId;
     uint256 public ticketCost;
     uint256 public currentTokenId;
 
