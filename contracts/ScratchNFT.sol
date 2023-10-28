f8e62c05ec12aab74f1a42f4c510bd0cf053c98c
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-10-28 22:09:59 +0700

    add custom error

--- contracts/ScratchNFT.sol
+++ contracts/ScratchNFT.sol
@@ -4,11 +4,14 @@ pragma solidity =0.8.21;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+error InvalidTokenId();
 
 contract ScratchNFT is ERC721Enumerable, Ownable {
+
     using Strings for uint256;
 
-    // Mapping to store the revealed property for each token ID
     mapping(uint256 => bool) public claimed;
     mapping(uint256 => string) public tokenURIs;
     mapping(uint256 => uint256) public editions;
