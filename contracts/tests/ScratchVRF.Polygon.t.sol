613b38b634b3b45657a78b704274ccd6c8ee365c
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-10-29 22:42:28 +0700

    rename test function

--- contracts/tests/ScratchVRF.Polygon.t.sol
+++ contracts/tests/ScratchVRF.Polygon.t.sol
@@ -38,7 +38,7 @@ contract TestScratchVRF_POLYGON is Test {
         );
     }
 
-    function testSomething()
+    function testChangeTicketCost()
         public
     {
         uint256 initialCost = scratcher.ticketCost();
