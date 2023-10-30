1ab48809533512840cd50297e00b1dde488bbd04
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-10-30 22:54:13 +0700

    remove webapp

--- webapp/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import VueClipboard from 'vue3-clipboard'
-const app = createApp(App)
-
-app.use(router)
-app.use(VueClipboard)
-app.mount('#app')
