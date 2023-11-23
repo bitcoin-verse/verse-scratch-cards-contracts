dba40343b32aaad4633767c83f07f2abe0d8a643
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2023-11-23 23:33:53 +0700

    add proper pausable

---
+++ contracts/helpers/Pausable.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: -- BCOM --
+
+pragma solidity =0.8.21;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+error ContractPaused();
+error ContractNotPaused();
+
+contract Pausable is Ownable {
+
+    bool public paused;
+
+    event Paused(
+        address indexed caller,
+        uint256 blocktime
+    );
+
+    event Unpaused(
+        address indexed caller,
+        uint256 blocktime
+    );
+
+    constructor() {
+        paused = false;
+    }
+
+    modifier whenNotPaused() {
+        if (paused == true) {
+            revert ContractPaused();
+        }
+        _;
+    }
+
+    modifier whenPaused() {
+        if (paused == false) {
+            revert ContractNotPaused();
+        }
+        _;
+    }
+
+    function pauseContract()
+        external
+        onlyOwner
+        whenNotPaused
+    {
+        paused = true;
+
+        emit Paused(
+            msg.sender,
+            block.timestamp
+        );
+    }
+
+    function unpauseContract()
+        external
+        onlyOwner
+        whenPaused
+    {
+        paused = false;
+
+        emit Unpaused(
+            msg.sender,
+            block.timestamp
+        );
+    }
+}
