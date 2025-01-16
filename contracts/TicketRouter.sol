6d43ea8a41c2218c11b33671ba907c547888ac63
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-01-16 16:11:11 +0700

    add swapExactTokensForTokens

--- contracts/TicketRouter.sol
+++ contracts/TicketRouter.sol
@@ -37,6 +37,16 @@ interface IUniswapV2Router02 {
         external
         payable
         returns (uint256[] memory amounts);
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (uint256[] memory amounts);
 }
 
 contract TicketRouter {
@@ -45,6 +55,13 @@ contract TicketRouter {
 
     address public immutable WETH;
     address public immutable VERSE_TOKEN;
+
+    event TokenPurchase(
+        address indexed buyer,
+        address indexed token,
+        uint256 amount,
+        uint256 receivedAmount
+    );
 
     constructor(
         address _uniswapRouter,
@@ -59,7 +76,7 @@ contract TicketRouter {
     }
 
     function getETHPriceForTickets(
-        address scratcherContract,
+        address _scratcherContract,
         uint256 _ticketCount
     )
         public
@@ -67,7 +84,7 @@ contract TicketRouter {
         returns (uint256 ethAmount)
     {
         ScratchVRF scratcher = ScratchVRF(
-            scratcherContract
+            _scratcherContract
         );
 
         uint256 totalCost = scratcher.baseCost() * _ticketCount;
@@ -85,7 +102,7 @@ contract TicketRouter {
     }
 
     function buyTicketsWithETH(
-        address scratcherContract,
+        address _scratcherContract,
         uint256 _ticketCount
     )
         external
@@ -97,11 +114,11 @@ contract TicketRouter {
         );
 
         ScratchVRF scratcher = ScratchVRF(
-            scratcherContract
+            _scratcherContract
         );
 
         uint256 ethRequired = getETHPriceForTickets(
-            scratcherContract,
+            _scratcherContract,
             _ticketCount
         );
 
@@ -134,13 +151,20 @@ contract TicketRouter {
         }
 
         IERC20(VERSE_TOKEN).approve(
-            scratcherContract,
+            _scratcherContract,
             totalCost
         );
 
         scratcher.bulkPurchase(
             msg.sender,
             _ticketCount
+        );
+
+        emit TokenPurchase(
+            msg.sender,
+            WETH,
+            msg.value,
+            swapAmount
         );
     }
 
@@ -175,6 +199,120 @@ contract TicketRouter {
         scratcher.bulkPurchase(
             msg.sender,
             _ticketCount
+        );
+
+        emit TokenPurchase(
+            msg.sender,
+            VERSE_TOKEN,
+            totalCost,
+            totalCost
+        );
+    }
+
+    function getTokenPriceForTickets(
+        address _scratcherContract,
+        uint256 _ticketCount,
+        address _inputToken
+    )
+        public
+        view
+        returns (uint256 tokenAmount)
+    {
+        ScratchVRF scratcher = ScratchVRF(
+            _scratcherContract
+        );
+
+        uint256 totalCost = scratcher.baseCost() * _ticketCount;
+
+        address[] memory path = new address[](2);
+        path[0] = _inputToken;
+        path[1] = VERSE_TOKEN;
+
+        uint256[] memory amounts = uniswapRouter.getAmountsIn(
+            totalCost,
+            path
+        );
+
+        return amounts[0];
+    }
+
+    function buyWithToken(
+        address _scratcherContract,
+        uint256 _ticketCount,
+        address _inputToken,
+        uint256 _maxTokenAmount
+    )
+        external
+    {
+        require(
+            _ticketCount > 0,
+            "Ticket count must be greater than 0"
+        );
+
+        uint256 tokenRequired = getTokenPriceForTickets(
+            _scratcherContract,
+            _ticketCount,
+            _inputToken
+        );
+
+        require(
+            _maxTokenAmount >= tokenRequired,
+            "Insufficient token amount sent"
+        );
+
+        ScratchVRF scratcher = ScratchVRF(
+            _scratcherContract
+        );
+
+        uint256 totalCost = scratcher.baseCost() * _ticketCount;
+
+        address[] memory path = new address[](2);
+        path[0] = _inputToken;
+        path[1] = VERSE_TOKEN;
+
+        IERC20(_inputToken).transferFrom(
+            msg.sender,
+            address(this),
+            _maxTokenAmount
+        );
+
+        IERC20(_inputToken).approve(
+            address(uniswapRouter),
+            _maxTokenAmount
+        );
+
+        uint256[] memory amounts = uniswapRouter.swapExactTokensForTokens(
+            _maxTokenAmount,
+            totalCost,
+            path,
+            address(this),
+            block.timestamp
+        );
+
+        uint256 tokensUsed = amounts[0];
+
+        if (_maxTokenAmount > tokensUsed) {
+            IERC20(_inputToken).transfer(
+                msg.sender,
+                _maxTokenAmount - tokensUsed
+            );
+        }
+
+        IERC20(VERSE_TOKEN).approve(
+            _scratcherContract,
+            totalCost
+        );
+
+        scratcher.bulkPurchase(
+            msg.sender,
+            _ticketCount
+        );
+
+        emit TokenPurchase(
+            msg.sender,
+            _inputToken,
+            _maxTokenAmount,
+            tokensUsed
         );
     }
 }
